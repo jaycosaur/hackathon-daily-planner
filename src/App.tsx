cc810c4177febd70a60ceb83a7371e4c57e9438c
Jacob Richter <jaycorichter@gmail.com>
2021-06-17 13:16:57 +1000

    full page map

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 import Map from "./map";
@@ -13,23 +11,8 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div className="App" style={{ width, height }}>
-          <Map width={width} height={height} polygons={[]} />
-        </div>
-      </header>
+    <div className="App" style={{ width, height }}>
+      <Map width={width} height={height} polygons={[]} />
     </div>
   );
 }
