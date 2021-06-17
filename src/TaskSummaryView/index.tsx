7d52eb366863303a532421d29e2f2b940d87de7c
Jacob Richter <jaycorichter@gmail.com>
2021-06-17 19:12:39 +1000

    optional search

--- src/TaskSummaryView/index.tsx
+++ src/TaskSummaryView/index.tsx
@@ -1,19 +1,22 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "./style.css";
-import {getStatusIcon, ITask, taskStatusAsChip, taskStatusAsColor, taskStatusAsString} from "../types/ITask";
-import {Button, Chip} from "@material-ui/core";
-import FaceIcon from '@material-ui/icons/Face';
-import {AppContext} from "../AppHandler/AppContext";
+import { ITask, taskStatusAsChip } from "../types/ITask";
+import { Button, Chip } from "@material-ui/core";
+import FaceIcon from "@material-ui/icons/Face";
+import { AppContext } from "../AppHandler/AppContext";
 import TaskOnMap from "../TaskAddEditView/TaskOnMap";
-import {useWindowSize} from "../useWindowSize";
-import {TaskActivityCard} from "../TaskActivityCard";
+import { useWindowSize } from "../useWindowSize";
+import { TaskActivityCard } from "../TaskActivityCard";
 
 export interface ITaskSummaryViewProps {
   task: ITask;
   onEdit: () => void;
 }
 
-export const TaskSummaryView: React.FC<ITaskSummaryViewProps> = ({ task, onEdit }) => {
+export const TaskSummaryView: React.FC<ITaskSummaryViewProps> = ({
+  task,
+  onEdit,
+}) => {
   const { users, retrieveActivityForTask } = useContext(AppContext);
   const windowSize = useWindowSize();
 
@@ -22,48 +25,45 @@ export const TaskSummaryView: React.FC<ITaskSummaryViewProps> = ({ task, onEdit 
     return null;
   }
 
-  const activeUserForTask = users.find(x => x._id === task.assignedUserId);
+  const activeUserForTask = users.find((x) => x._id === task.assignedUserId);
 
   // TODO: use shared chip
-  return <div className="component-TaskSummaryView">
-    <h2>{task.title}</h2>
-    <div className="task-summary-ribbon">
-      {taskStatusAsChip(task.status)}
-      {
-        activeUserForTask &&
-        <Chip
+  return (
+    <div className="component-TaskSummaryView">
+      <h2>{task.title}</h2>
+      <div className="task-summary-ribbon">
+        {taskStatusAsChip(task.status)}
+        {activeUserForTask && (
+          <Chip
             icon={<FaceIcon />}
             label={activeUserForTask.email.split("@")[0]}
             color="primary"
             variant="outlined"
+          />
+        )}
+
+        <div className="spacer" />
+
+        <Button>Mark completed</Button>
+        <Button onClick={onEdit}>Edit</Button>
+      </div>
+      <div className="task-map">
+        <TaskOnMap
+          width={windowSize.width || 0}
+          height={200}
+          onClick={() => {}}
+          onDelete={() => {}}
+          point={task.location}
         />
-      }
+      </div>
 
-      <div className="spacer" />
-
-      <Button>Mark completed</Button>
-      <Button onClick={onEdit}>Edit</Button>
-    </div>
-    <div className="task-map">
-      <TaskOnMap
-        width={windowSize.width || 0}
-        height={200}
-        onClick={() => {}}
-        onDelete={() => {}}
-        point={task.location}
-      />
-    </div>
-
-    <div className="task-activities">
-      {
-        retrieveActivityForTask(task._id).map(taskActivity =>
+      <div className="task-activities">
+        {retrieveActivityForTask(task._id).map((taskActivity) => (
           <TaskActivityCard taskActivity={taskActivity} />
-        )
-      }
-    </div>
+        ))}
+      </div>
 
-    <div className="task-activity-bar">
-      Hello
+      <div className="task-activity-bar">Hello</div>
     </div>
-  </div>;
-}
+  );
+};
