50a879b739c9a536a5239484f3451d2c322dac63
Jacob Richter <jaycorichter@gmail.com>
2021-06-17 20:55:10 +1000

    optional search

--- src/FilterControls/index.tsx
+++ src/FilterControls/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import SearchIcon from "@material-ui/icons/Search";
