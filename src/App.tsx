5582ee4038a707ef906ffcc461412c429f82bb09
Jye Lewis <jye.lewis@propelleraero.com>
2021-06-17 14:56:53 +1000

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,68 @@
+import React, {useCallback, useContext, useState} from "react";
 import "./App.css";
-import React from "react";
+import {v4 as uuid} from "uuid";
+import {AppBar, CircularProgress, IconButton} from "@material-ui/core";
+import Map from "./map";
+import {useWindowSize} from "./useWindowSize";
+import {ThemeProvider} from "@material-ui/core";
+import {lightTheme} from "./theme/themes";
+import MapIcon from '@material-ui/icons/Map';
+import ListIcon from '@material-ui/icons/List';
+import {AppContext} from "./AppHandler/AppContext";
 
-// import TaskView from "./TaskView";
-import MapView from "./MapView";
-import { useWindowSize } from "./useWindowSize";
-import { ThemeProvider } from "@material-ui/core";
+const appBarHeight = 50;
 
-import { lightTheme } from "./theme/themes";
-
-function App() {
+// TODO: show spinner while loading
+export const App: React.FC = () => {
   const { width, height } = useWindowSize();
 
-  // const { isLoading, users, activeUser, login } = useContext(AppContext);
-  // console.log(isLoading, users, activeUser);
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     login("jye.lewis@propelleraero.com.au")
-  //       .then(() => console.log("Logged in"))
-  //       .catch(console.error);
-  //   }, 1000);
-  // }, []);
+  const [showingTaskList, setShowingTaskList] = useState(false);
+  const toggleTaskList = useCallback(() => {
+    setShowingTaskList(x => !x);
+  }, [setShowingTaskList]);
 
   if (!width || !height) {
     return null;
   }
 
-  return (
-    <ThemeProvider theme={lightTheme}>
-      <div className="App" style={{ width, height }}>
-        <MapView width={width} height={height} />
-      </div>
-    </ThemeProvider>
-  );
-}
+  const [points, setPoints] = React.useState<
+    Array<{
+      latitude: number;
+      longitude: number;
+      id: string;
+    }>
+    >([
+    {
+      latitude: -33.881144323948234,
+      longitude: 151.2135851533549,
+      id: "1",
+    },
+  ]);
 
-export default App;
+  return <ThemeProvider theme={lightTheme}>
+    <div className="App" style={{width, height}}>
+    <AppBar position="fixed" className="main-app-bar" style={{ height: appBarHeight }}>
+      <IconButton
+        color="inherit"
+        aria-label="Task list"
+        onClick={toggleTaskList}
+      >
+        {
+          showingTaskList ? <ListIcon/> : <MapIcon/>
+        }
+      </IconButton>
+    </AppBar>
+    <Map
+      width={width}
+      height={height}
+      points={points}
+      onClickPoint={(pt) =>
+        setPoints((old) => [...old, { id: uuid(), ...pt }])
+      }
+      onPointSelected={(pt) =>
+        setPoints((old) => old.filter((old) => old.id !== pt.id))
+      }
+    />
+    </div>
+  </ThemeProvider>
+}
