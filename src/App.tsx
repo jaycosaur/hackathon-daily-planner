3123e11dabea82f941ed2623b6cc8cea527c0beb
Jacob Richter <jaycorichter@gmail.com>
2021-06-17 14:21:54 +1000

    add map play

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,57 @@
 import "./App.css";
+import React from "react";
+import { v4 as uuid } from "uuid";
 
 import Map from "./map";
 import TaskView from "./TaskView";
 import { useWindowSize } from "./useWindowSize";
 
 function App() {
-    const { width, height } = useWindowSize();
+  const { width, height } = useWindowSize();
 
-    // const { isLoading, users, activeUser, login } = useContext(AppContext);
-    // console.log(isLoading, users, activeUser);
-    // useEffect(() => {
-    //   setTimeout(() => {
-    //     login("jye.lewis@propelleraero.com.au")
-    //       .then(() => console.log("Logged in"))
-    //       .catch(console.error);
-    //   }, 1000);
-    // }, []);
+  // const { isLoading, users, activeUser, login } = useContext(AppContext);
+  // console.log(isLoading, users, activeUser);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     login("jye.lewis@propelleraero.com.au")
+  //       .then(() => console.log("Logged in"))
+  //       .catch(console.error);
+  //   }, 1000);
+  // }, []);
 
-    if (!width || !height) {
-        return null;
-    }
+  const [points, setPoints] = React.useState<
+    Array<{
+      latitude: number;
+      longitude: number;
+      id: string;
+    }>
+  >([
+    {
+      latitude: -33.881144323948234,
+      longitude: 151.2135851533549,
+      id: "1",
+    },
+  ]);
 
-    return (
-        <div className="App" style={{ width, height }}>
-            <Map
-                width={width}
-                height={height}
-                points={[
-                    {
-                        latitude: -33.881144323948234,
-                        longitude: 151.2135851533549,
-                        id: "1",
-                    },
-                ]}
-            />
-        </div>
-    );
+  if (!width || !height) {
+    return null;
+  }
+
+  return (
+    <div className="App" style={{ width, height }}>
+      <Map
+        width={width}
+        height={height}
+        points={points}
+        onClickPoint={(pt) =>
+          setPoints((old) => [...old, { id: uuid(), ...pt }])
+        }
+        onPointSelected={(pt) =>
+          setPoints((old) => old.filter((old) => old.id !== pt.id))
+        }
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/map/TaskOnMap.tsx
+++ src/map/TaskOnMap.tsx
@@ -1,1 +1,29 @@
-export {};
+import React from "react";
+import { ITask } from "../types/ITask";
+
+import Map from "../map";
+
+const TaskOnMap: React.FC<{ task: ITask; width: number; height: number }> = ({
+  width,
+  height,
+  task,
+}) => {
+  if (!task.location) {
+    return <Map width={width} height={height} />;
+  }
+  return (
+    <Map
+      width={width}
+      height={height}
+      points={[
+        {
+          id: task._id,
+          latitude: task.location.lat,
+          longitude: task.location.long,
+        },
+      ]}
+    />
+  );
+};
+
+export default TaskOnMap;
