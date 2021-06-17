ca1838d7aebe92a8dc69099fc3a39b536fc74949
Jacob Richter <jaycorichter@gmail.com>
2021-06-17 15:12:25 +1000

    theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,37 @@
 import "./App.css";
 import React from "react";
-import { v4 as uuid } from "uuid";
 
-import Map from "./map";
-import TaskView from "./TaskView";
+// import TaskView from "./TaskView";
+import MapView from "./MapView";
 import { useWindowSize } from "./useWindowSize";
 import { ThemeProvider } from "@material-ui/core";
 
 import { lightTheme } from "./theme/themes";
 
 function App() {
-    const { width, height } = useWindowSize();
+  const { width, height } = useWindowSize();
 
-    // const { isLoading, users, activeUser, login } = useContext(AppContext);
-    // console.log(isLoading, users, activeUser);
-    // useEffect(() => {
-    //   setTimeout(() => {
-    //     login("jye.lewis@propelleraero.com.au")
-    //       .then(() => console.log("Logged in"))
-    //       .catch(console.error);
-    //   }, 1000);
-    // }, []);
+  // const { isLoading, users, activeUser, login } = useContext(AppContext);
+  // console.log(isLoading, users, activeUser);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     login("jye.lewis@propelleraero.com.au")
+  //       .then(() => console.log("Logged in"))
+  //       .catch(console.error);
+  //   }, 1000);
+  // }, []);
 
-    const [points, setPoints] = React.useState<
-        Array<{
-            latitude: number;
-            longitude: number;
-            id: string;
-        }>
-    >([
-        {
-            latitude: -33.881144323948234,
-            longitude: 151.2135851533549,
-            id: "1",
-        },
-    ]);
+  if (!width || !height) {
+    return null;
+  }
 
-    if (!width || !height) {
-        return null;
-    }
-
-    return (
-        <ThemeProvider theme={lightTheme}>
-            <div className="App" style={{ width, height }}>
-                <Map
-                    width={width}
-                    height={height}
-                    points={points}
-                    onClickPoint={(pt) =>
-                        setPoints((old) => [...old, { id: uuid(), ...pt }])
-                    }
-                    onPointSelected={(pt) =>
-                        setPoints((old) =>
-                            old.filter((old) => old.id !== pt.id)
-                        )
-                    }
-                />
-            </div>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <div className="App" style={{ width, height }}>
+        <MapView width={width} height={height} />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
