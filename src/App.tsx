af09ee1bf046fe5ad50d34e667c7fc7cefe5dc96
Jesse Gibbs <jesse.gibbs@propelleraero.com.au>
2021-06-17 14:17:20 +1000

    fixed it

--- src/App.tsx
+++ src/App.tsx
@@ -5,42 +5,37 @@ import TaskView from "./TaskView";
 import { useWindowSize } from "./useWindowSize";
 
 function App() {
-  const { width, height } = useWindowSize();
+    const { width, height } = useWindowSize();
 
-  // const { isLoading, users, activeUser, login } = useContext(AppContext);
-  // console.log(isLoading, users, activeUser);
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     login("jye.lewis@propelleraero.com.au")
-  //       .then(() => console.log("Logged in"))
-  //       .catch(console.error);
-  //   }, 1000);
-  // }, []);
+    // const { isLoading, users, activeUser, login } = useContext(AppContext);
+    // console.log(isLoading, users, activeUser);
+    // useEffect(() => {
+    //   setTimeout(() => {
+    //     login("jye.lewis@propelleraero.com.au")
+    //       .then(() => console.log("Logged in"))
+    //       .catch(console.error);
+    //   }, 1000);
+    // }, []);
 
-  if (!width || !height) {
-    return null;
-  }
+    if (!width || !height) {
+        return null;
+    }
 
-  return (
-    <div className="App" style={{ width, height }}>
-<<<<<<< HEAD
-      {/* <Map width={width} height={height} polygons={[]} /> */}
-      <TaskView></TaskView>
-=======
-      <Map
-        width={width}
-        height={height}
-        points={[
-          {
-            latitude: -33.881144323948234,
-            longitude: 151.2135851533549,
-            id: "1",
-          },
-        ]}
-      />
->>>>>>> b0e4db482f8709ddbe6da8b505f48dd7e84747a0
-    </div>
-  );
+    return (
+        <div className="App" style={{ width, height }}>
+            <Map
+                width={width}
+                height={height}
+                points={[
+                    {
+                        latitude: -33.881144323948234,
+                        longitude: 151.2135851533549,
+                        id: "1",
+                    },
+                ]}
+            />
+        </div>
+    );
 }
 
 export default App;
