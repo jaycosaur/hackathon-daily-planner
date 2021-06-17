1d4c49ee35778526da805d26ec5fe08443e0d61e
Jacob Richter <jaycorichter@gmail.com>
2021-06-17 21:41:42 +1000

    optional search

--- src/AppHandler/index.tsx
+++ src/AppHandler/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { AppContext } from "./AppContext";
 import { IAppContextValue } from "../types/IAppContextValue";
