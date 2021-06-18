1d58059f5fa2f2eb98e84df187f6c005f52c8949
Jesse Gibbs <jesse.gibbs@propelleraero.com.au>
2021-06-18 10:39:45 +1000

    done

--- src/TaskActivityCard/index.tsx
+++ src/TaskActivityCard/index.tsx
@@ -1,7 +1,7 @@
-import {ETaskActivityType, ITaskActivity} from "../types/ITaskActivity";
-import React, {useContext, useEffect, useState} from "react";
-import {AppContext} from "../AppHandler/AppContext";
-import {ITaskImage} from "../types/ITaskImage";
+import { ETaskActivityType, ITaskActivity } from "../types/ITaskActivity";
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../AppHandler/AppContext";
+import { ITaskImage } from "../types/ITaskImage";
 import {
   Avatar,
   ListItem,
@@ -15,7 +15,7 @@ export interface ITaskActivityCardProps {
 }
 
 function useTaskImage(imageId?: string): undefined | ITaskImage {
-  const {retrieveTaskImage} = useContext(AppContext);
+  const { retrieveTaskImage } = useContext(AppContext);
   const [taskImage, setTaskImage] = useState<undefined | ITaskImage>(undefined);
 
   useEffect(() => {
@@ -36,23 +36,27 @@ function useTaskImage(imageId?: string): undefined | ITaskImage {
   return taskImage;
 }
 
-const ChatBubble: React.FC<{ text: string; email: string, date: string }> = ({
-                                                                               text,
-                                                                               email,
-                                                                               date
-                                                                             }) => (
+const ChatBubble: React.FC<{ text: string; email: string; date: string }> = ({
+  text,
+  email,
+  date,
+}) => (
   <ListItem alignItems="flex-start">
     <ListItemAvatar>
       <Avatar>{email[0].toUpperCase()}</Avatar>
     </ListItemAvatar>
     <ListItemText
       primary={email.split("@")[0] + ` (${date})`}
-      secondary={<React.Fragment><pre>{text.trim()}</pre></React.Fragment>}
+      secondary={
+        <React.Fragment>
+          <pre>{text.trim()}</pre>
+        </React.Fragment>
+      }
     />
   </ListItem>
 );
 
-const StatusBubble: React.FC<{ text: string }> = ({text}) => {
+const StatusBubble: React.FC<{ text: string }> = ({ text }) => {
   return (
     <ListItem alignItems="flex-start">
       <ListItemText
@@ -63,40 +67,67 @@ const StatusBubble: React.FC<{ text: string }> = ({text}) => {
   );
 };
 
-const ImageBubble: React.FC<{ src: string; email: string, date: string }> = ({
-                                                                               src,
-                                                                               email,
-                                                                               date
-                                                                             }) => (
+const ImageBubble: React.FC<{ src: string; email: string; date: string }> = ({
+  src,
+  email,
+  date,
+}) => (
   <ListItem alignItems="flex-start">
     <ListItemAvatar>
       <Avatar>{email[0].toUpperCase()}</Avatar>
     </ListItemAvatar>
     <ListItemText
       primary={email.split("@")[0] + ` (${date})`}
-      secondary={<img src={src} alt="" width={"50%"}/>}
+      secondary={<img src={src} alt="" width={"50%"} />}
+    />
+  </ListItem>
+);
+
+const AudioBubble: React.FC<{ src: string; email: string; date: string }> = ({
+  src,
+  email,
+  date,
+}) => (
+  <ListItem alignItems="flex-start">
+    <ListItemAvatar>
+      <Avatar>{email[0].toUpperCase()}</Avatar>
+    </ListItemAvatar>
+    <ListItemText
+      primary={email.split("@")[0] + ` (${date})`}
+      secondary={
+        <>
+          <audio controls>
+            <source src={src} type="audio/mp3" />
+          </audio>
+        </>
+      }
     />
   </ListItem>
 );
 
 export const TaskActivityCard: React.FC<ITaskActivityCardProps> = ({
-                                                                     taskActivity,
-                                                                   }) => {
+  taskActivity,
+}) => {
   // load image, if we have one
   const taskImage = useTaskImage(taskActivity.taskImageId);
 
-  const {users} = useContext(AppContext);
+  const { users } = useContext(AppContext);
 
   if (taskActivity.type === ETaskActivityType.Text) {
-    return <ChatBubble
-      text={taskActivity.text}
-      email={users.find(x => x._id === taskActivity.createdByUserId)?.email || "[Unknown]"}
-      date={moment.unix(taskActivity.createdAt).fromNow()}
-    />;
+    return (
+      <ChatBubble
+        text={taskActivity.text}
+        email={
+          users.find((x) => x._id === taskActivity.createdByUserId)?.email ||
+          "[Unknown]"
+        }
+        date={moment.unix(taskActivity.createdAt).fromNow()}
+      />
+    );
   }
 
   if (taskActivity.type === ETaskActivityType.StatusUpdate) {
-    return <StatusBubble text={taskActivity.text}/>;
+    return <StatusBubble text={taskActivity.text} />;
   }
 
   if (taskActivity.type === ETaskActivityType.Image) {
@@ -105,12 +136,28 @@ export const TaskActivityCard: React.FC<ITaskActivityCardProps> = ({
       return null;
     }
 
-    return (
-      <ImageBubble
-        src={taskImage.data}
-        email={users.find(x => x._id === taskActivity.createdByUserId)?.email || "[Unknown]"}
-        date={moment.unix(taskActivity.createdAt).fromNow()}
-      />
-    );
+    if (taskImage.audioData) {
+      return (
+        <AudioBubble
+          src={taskImage.audioData}
+          email={
+            users.find((x) => x._id === taskActivity.createdByUserId)?.email ||
+            "[Unknown]"
+          }
+          date={moment.unix(taskActivity.createdAt).fromNow()}
+        />
+      );
+    } else {
+      return (
+        <ImageBubble
+          src={taskImage.data}
+          email={
+            users.find((x) => x._id === taskActivity.createdByUserId)?.email ||
+            "[Unknown]"
+          }
+          date={moment.unix(taskActivity.createdAt).fromNow()}
+        />
+      );
+    }
   }
 };
